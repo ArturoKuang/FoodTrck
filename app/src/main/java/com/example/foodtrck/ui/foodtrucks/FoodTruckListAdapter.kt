c96d3dc5b019bf5eeaacc0424de56b3e77891c3c
axk4647 <arturokuang@gmail.com>
2020-11-17 19:21:05 -0500

    we can now load images of food trucks

--- app/src/main/java/com/example/foodtrck/ui/foodtrucks/FoodTruckListAdapter.kt
+++ app/src/main/java/com/example/foodtrck/ui/foodtrucks/FoodTruckListAdapter.kt
@@ -4,6 +4,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.example.foodtrck.data.model.FoodTruck
 import com.example.foodtrck.databinding.FoodtruckListItemBinding
 
@@ -61,6 +63,11 @@ class FoodTruckListAdapter(private var listener: FoodTruckItemListener)
         fun bind(item: FoodTruck) {
             foodTruck = item
             itemBinding.foodtruckName.text = foodTruck.name
+
+            Glide.with(itemBinding.root)
+                .load(item.images?.header?.first())
+                .transform(CircleCrop())
+                .into(itemBinding.foodtruckImage)
         }
 
         override fun onClick(v: View?) {
